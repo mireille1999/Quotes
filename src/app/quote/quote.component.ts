9476ba1ddcf7bd09fdbcaa5b0cfd41eb7724374f
mireille1999 <muhawenimana920@daviscollege.com>
2021-02-19 14:25:01 +0200

    added bootstramp

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,3 +1,4 @@
+import { Quote } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,12 +10,12 @@ export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
     new Quote(1, '"Do not settle for less."', 'Mireille', 'Mireille', new Date(2021,2,19), 0, 0),
     new Quote(2, '"Make everyday a little less ordinary."', 'Amanda', 'Amanda', new Date(2021,2,20), 0, 0),
-    new Quote(3, '"You did not come this far to only come this far."', 'unknown', 'Benitha', new Date(2020,4,11), 0, 0),
+    new Quote(3, '"You did not come this far to only come this far."', 'unknown', 'Fidela', new Date(2021,1,1), 0, 0),
   ];
-  toggleDetails(index){
+  toggleDetails(index : any){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
-  deleteQuote(isComplete, index){
+  deleteQuote(isComplete : any, index : any){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].quote}?`)
       if (toDelete){
@@ -22,7 +23,7 @@ export class QuoteComponent implements OnInit {
       }
     }
   }
-  addNewQuote(quote){
+  addNewQuote(quote: any){
     let quoteLength = this.quotes.length;
     quote.id = quoteLength+1;
     quote.completeDate = new Date(quote.completeDate)

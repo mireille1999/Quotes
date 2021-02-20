e9eaa136495ed07297e7449efe1a0a767f4b4233
mireille1999 <muhawenimana920@daviscollege.com>
2021-02-20 08:25:54 +0200

    made some changes

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-
+  newQuote = new Quote(0,"","","",new Date(),0,0);
+  @Output() addQuote= new EventEmitter<Quote>();
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+  }
   constructor() { }
-
   ngOnInit(): void {
   }
-
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
-import { Quote } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
